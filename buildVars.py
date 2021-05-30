cdc1c627aedbda3f2f27e437ef4d4c6c9c2dd6b7
Javi Dominguez <javidominguez_github@outlook.es>
2021-05-30 13:58:09 +0200

    Tested in NVDA 2021.1: Compatibility OK. Updated manifest.

--- buildVars.py
+++ buildVars.py
@@ -34,7 +34,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("Provides some accessibility enhancements for the interface of Calibre eBook Management"),
 	# version
-	"addon_version" : "2.2",
+	"addon_version" : "2.3",
 	# Author(s)
 	"addon_author" : u"Javi Dominguez <fjavids@gmail.com>",
 	# URL for the add-on documentation support
@@ -44,7 +44,7 @@ addon_info = {
 	# Minimum NVDA version supported (e.g. "2018.3")
 	"addon_minimumNVDAVersion" : "2018.1.0",
 	# Last NVDA version supported/tested (e.g. "2018.4", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion" : "2021.0.0",
+	"addon_lastTestedNVDAVersion" : "2021.1.0",
 	# Add-on update channel (default is stable or None)
 	"addon_updateChannel" : None
 }
